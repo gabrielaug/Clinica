c01fe435f7d5c05c161895fdf95b7f8a5a20cde5
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2018-11-11 22:40:24 -0300

    Update Biblioteca/negocio/RNAtendimento.cs

---
+++ Biblioteca/negocio/RNAtendimento.cs
@@ -0,0 +1,41 @@
+﻿using Biblioteca.Basicas;
+using Biblioteca.dados;
+using Biblioteca.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.negocio
+{
+    public class RNAtendimento: InterfaceAtendimento
+    {
+        DAOAtendimento dao;
+
+        public RNAtendimento()
+        {
+            dao = new DAOAtendimento();
+        }
+
+        public void atualizar(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void excluir(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void gerar(Atendimento atendimento)
+        {
+            if()
+        }
+
+        public List<Atendimento> listarAtendimentos(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
