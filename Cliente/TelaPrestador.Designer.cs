27f7705d68b78d05bc221e8c45a10890369aabfa
joannaarruda <38672980+joannaarruda@users.noreply.github.com>
2018-11-18 19:25:38 -0300

    TelaPrestador
    Segue a tela de cadastro de prestadors

---
+++ Cliente/TelaPrestador.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Cliente
+{
+    partial class TelaPrestador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CdPrestador = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NmPrestador = new System.Windows.Forms.TextBox();
+            this.CPF = new System.Windows.Forms.Label();
+            this.cpfprestador = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NrConselho = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TelefonePrestador = new System.Windows.Forms.TextBox();
+            this.CADASTRAR = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Prestadores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CÓDIGO";
+            // 
+            // CdPrestador
+            // 
+            this.CdPrestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CdPrestador.Location = new System.Drawing.Point(96, 91);
+            this.CdPrestador.Name = "CdPrestador";
+            this.CdPrestador.Size = new System.Drawing.Size(100, 30);
+            this.CdPrestador.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NOME";
+            // 
+            // NmPrestador
+            // 
+            this.NmPrestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NmPrestador.Location = new System.Drawing.Point(96, 136);
+            this.NmPrestador.Name = "NmPrestador";
+            this.NmPrestador.Size = new System.Drawing.Size(736, 30);
+            this.NmPrestador.TabIndex = 4;
+            // 
+            // CPF
+            // 
+            this.CPF.AutoSize = true;
+            this.CPF.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPF.Location = new System.Drawing.Point(12, 178);
+            this.CPF.Name = "CPF";
+            this.CPF.Size = new System.Drawing.Size(43, 24);
+            this.CPF.TabIndex = 5;
+            this.CPF.Text = "CPF";
+            // 
+            // cpfprestador
+            // 
+            this.cpfprestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpfprestador.Location = new System.Drawing.Point(96, 175);
+            this.cpfprestador.Name = "cpfprestador";
+            this.cpfprestador.Size = new System.Drawing.Size(199, 30);
+            this.cpfprestador.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(301, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "NR CONSELHO";
+            // 
+            // NrConselho
+            // 
+            this.NrConselho.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NrConselho.Location = new System.Drawing.Point(439, 178);
+            this.NrConselho.Name = "NrConselho";
+            this.NrConselho.Size = new System.Drawing.Size(111, 30);
+            this.NrConselho.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(556, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TELEFONE";
+            // 
+            // TelefonePrestador
+            // 
+            this.TelefonePrestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TelefonePrestador.Location = new System.Drawing.Point(650, 181);
+            this.TelefonePrestador.Name = "TelefonePrestador";
+            this.TelefonePrestador.Size = new System.Drawing.Size(182, 30);
+            this.TelefonePrestador.TabIndex = 10;
+            // 
+            // CADASTRAR
+            // 
+            this.CADASTRAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CADASTRAR.Location = new System.Drawing.Point(887, 90);
+            this.CADASTRAR.Name = "CADASTRAR";
+            this.CADASTRAR.Size = new System.Drawing.Size(151, 31);
+            this.CADASTRAR.TabIndex = 11;
+            this.CADASTRAR.Text = "CADASTRAR";
+            this.CADASTRAR.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(887, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 31);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(887, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "INATIVAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(887, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 68);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "CONSULTAR / LISTAR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(16, 217);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(816, 257);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // TelaPrestador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 483);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CADASTRAR);
+            this.Controls.Add(this.TelefonePrestador);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.NrConselho);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cpfprestador);
+            this.Controls.Add(this.CPF);
+            this.Controls.Add(this.NmPrestador);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CdPrestador);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TelaPrestador";
+            this.Text = "TelaPrestador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CdPrestador;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NmPrestador;
+        private System.Windows.Forms.Label CPF;
+        private System.Windows.Forms.TextBox cpfprestador;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NrConselho;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TelefonePrestador;
+        private System.Windows.Forms.Button CADASTRAR;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
