c3469bcc4d996f602ddc214016237e0784e9ca3e
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2018-11-18 18:31:43 -0300

    WCF Correção

--- ServidorWCF/IService1.cs
+++ ServidorWCF/IService1.cs
@@ -13,85 +13,85 @@ namespace ServidorWCF
     public interface IService1
     {
         [OperationContract]
-        void Agendar(Agendamento agendamento);
+        void AgendarAgendamento(Agendamento agendamento);
 
         [OperationContract]
-        void Remarcar(Agendamento agendamento);
+        void RemarcarAgendamento(Agendamento agendamento);
 
         [OperationContract]
-        void Excluir(Agendamento agendamento);
+        void ExcluirAgendamento(Agendamento agendamento);
 
         [OperationContract]
         List<Agendamento> ListaAgendamentos(Agendamento agendamento);
 
         [OperationContract]
-        void gerar(Atendimento atendimento);
+        void GerarAtendimento(Atendimento atendimento);
 
         [OperationContract]
-        void excluir(Atendimento atendimento);
+        void ExcluirAtendimento(Atendimento atendimento);
 
         [OperationContract]
-        void atualizar(Atendimento atendimento);
+        void AtualizarAtendimento(Atendimento atendimento);
 
         [OperationContract]
-        List<Atendimento> listarAtendimentos(Atendimento atendimento);
+        List<Atendimento> ListarAtendimentos(Atendimento atendimento);
 
         [OperationContract]
-        void cadastrar(Convenio convenio);
+        void CadastrarConvenio(Convenio convenio);
 
         [OperationContract]
-        void inativar(Convenio convenio);
+        void InativarConvenio(Convenio convenio);
 
         [OperationContract]
-        List<Convenio> listarConvenios(Convenio filtro);
+        List<Convenio> ListarConvenios(Convenio filtro);
 
         [OperationContract]
-        void Cadastrar(Paciente paciente);
+        void CadastrarPaciente(Paciente paciente);
 
         [OperationContract]
-        void Alterar(Paciente paciente);
+        void AlterarPaciente(Paciente paciente);
 
         [OperationContract]
         List<Paciente> ListaPaciente(Paciente filtro);
 
         [OperationContract]
-        void cadastrar(Prestador prestador);
+        void CadastrarPrestador(Prestador prestador);
 
         [OperationContract]
-        void inativar(Prestador prestador);
+        void InativarPrestador(Prestador prestador);
 
         [OperationContract]
-        void atualizar(Prestador prestador);
+        void AtualizarPrestador(Prestador prestador);
 
         [OperationContract]
         List<Prestador> ListaPrestadores(Prestador filtro);
 
         [OperationContract]
-        void Adicionar(Procedimento procedimento);
+        void AdicionarProcedimento(Procedimento procedimento);
 
         [OperationContract]
-        void Atuaizar(Procedimento procedimento);
+        void AtuaizarProcedimento(Procedimento procedimento);
 
         [OperationContract]
         List<Procedimento> ListarProcedimento(Procedimento procedimento);
 
         [OperationContract]
-        void inserir(TipoConsulta tipoConsulta);
+        void InserirTipoConsulta(TipoConsulta tipoConsulta);
 
         [OperationContract]
-        void inativar(TipoConsulta tipoConsulta);
+        void InativarTipoConsulta(TipoConsulta tipoConsulta);
 
         [OperationContract]
-        List<TipoConsulta> listarTipoConsulta(TipoConsulta filtro);
+        List<TipoConsulta> ListarTipoConsulta(TipoConsulta filtro);
 
         [OperationContract]
-        void cadastrar(Usuario usuario);
+        void CadastrarUsuario(Usuario usuario);
 
         [OperationContract]
-        void alterar(Usuario usuario);
+        void AlterarUsuario(Usuario usuario);
 
         [OperationContract]
-        List<Usuario> listarUsuarios(Usuario filtro);
+        List<Usuario> ListarUsuarios(Usuario filtro);
 
     }
 
