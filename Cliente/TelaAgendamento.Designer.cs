e3c5c4b520c67faea76e42b050ff362345c9ccd5
joannaarruda <38672980+joannaarruda@users.noreply.github.com>
2018-11-18 21:47:28 -0300

    tela de agendamento
    t

---
+++ Cliente/TelaAgendamento.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Cliente
+{
+    partial class Tela_Agendamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CdAgendamento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DtConsulta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CdPaciente = new System.Windows.Forms.TextBox();
+            this.NmPaciente = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NmPrestador = new System.Windows.Forms.TextBox();
+            this.CdPrestador = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(495, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agendamento de Paciente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CODIGO";
+            // 
+            // CdAgendamento
+            // 
+            this.CdAgendamento.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CdAgendamento.Location = new System.Drawing.Point(135, 185);
+            this.CdAgendamento.Name = "CdAgendamento";
+            this.CdAgendamento.Size = new System.Drawing.Size(100, 30);
+            this.CdAgendamento.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(461, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "DATA DA CONSULTA";
+            // 
+            // DtConsulta
+            // 
+            this.DtConsulta.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DtConsulta.Location = new System.Drawing.Point(656, 82);
+            this.DtConsulta.Name = "DtConsulta";
+            this.DtConsulta.Size = new System.Drawing.Size(169, 30);
+            this.DtConsulta.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "PACIENTE";
+            // 
+            // CdPaciente
+            // 
+            this.CdPaciente.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CdPaciente.Location = new System.Drawing.Point(135, 224);
+            this.CdPaciente.Name = "CdPaciente";
+            this.CdPaciente.Size = new System.Drawing.Size(100, 30);
+            this.CdPaciente.TabIndex = 7;
+            // 
+            // NmPaciente
+            // 
+            this.NmPaciente.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NmPaciente.Location = new System.Drawing.Point(241, 224);
+            this.NmPaciente.Name = "NmPaciente";
+            this.NmPaciente.Size = new System.Drawing.Size(584, 30);
+            this.NmPaciente.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PRESTADOR";
+            // 
+            // NmPrestador
+            // 
+            this.NmPrestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NmPrestador.Location = new System.Drawing.Point(241, 270);
+            this.NmPrestador.Name = "NmPrestador";
+            this.NmPrestador.Size = new System.Drawing.Size(584, 30);
+            this.NmPrestador.TabIndex = 11;
+            // 
+            // CdPrestador
+            // 
+            this.CdPrestador.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CdPrestador.Location = new System.Drawing.Point(135, 270);
+            this.CdPrestador.Name = "CdPrestador";
+            this.CdPrestador.Size = new System.Drawing.Size(100, 30);
+            this.CdPrestador.TabIndex = 10;
+            // 
+            // Usuario
+            // 
+            this.Usuario.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(333, 185);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(204, 30);
+            this.Usuario.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(241, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "USUARIO";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "PACIENTE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(186, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 40);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "AGENDAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(361, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 40);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "REMARCAR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(716, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 40);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "EXCLUIR";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(534, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 40);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "LISTAR";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(16, 388);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(809, 224);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Tela_Agendamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 624);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.NmPrestador);
+            this.Controls.Add(this.CdPrestador);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NmPaciente);
+            this.Controls.Add(this.CdPaciente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DtConsulta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CdAgendamento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Tela_Agendamento";
+            this.Text = "Tela_Agendamento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CdAgendamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DtConsulta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CdPaciente;
+        private System.Windows.Forms.TextBox NmPaciente;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox NmPrestador;
+        private System.Windows.Forms.TextBox CdPrestador;
+        private System.Windows.Forms.TextBox Usuario;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
